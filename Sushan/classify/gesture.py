b6a01c8fd69e189325e8a6796af17de53b4e597e
Sushan Sapaliga <sushansapaliga@gmail.com>
2020-04-18 20:05:20 +0530

    Update gesture.py

--- Sushan/classify/gesture.py
+++ Sushan/classify/gesture.py
@@ -68,26 +68,39 @@ def __checkHandAboveShoulderStyle__(wrist, elbow, shoulder, innerGesture, outerG
 
     return None
 
+def __checkingIntersectionPoint__(R_elbow, R_wrist, L_elbow, L_wrist):
+    x, y = 0, 0
+    try:
+        slope_right = (R_elbow[1] - R_wrist[1]) / (R_elbow[0] - R_wrist[0])
+        coeffi_right = R_elbow[1] - slope_right * R_elbow[0]
+
+        slope_left = (L_elbow[1] - L_wrist[1]) / (L_elbow[0] - L_wrist[0])
+        coeffi_left = L_elbow[1] - slope_left * L_elbow[0]
+
+        x = (coeffi_left - coeffi_right) / (slope_right - slope_left)
+        y = slope_right * x + coeffi_right
+    except :
+        return 0, 0
+    finally:
+        return x, y
+    pass
+
 def __checkWakandaStyle__(R_elbow, R_wrist, L_elbow, L_wrist, neck, midHip, nose):
-    midRightHand = []
-    midRightHand.append( (R_elbow[0] + R_wrist[0])//2 )
-    midRightHand.append( (R_elbow[1] + R_wrist[1])//2 )
 
-    midLeftHand = []
-    midLeftHand.append( (L_elbow[0] + L_wrist[0])//2 )
-    midLeftHand.append( (L_elbow[1] + L_wrist[1])//2 )
-
-    x = pow((midRightHand[0] - midLeftHand[0]), 2)
-    y = pow((midRightHand[1] - midLeftHand[1]), 2)
+    x, y = __checkingIntersectionPoint__(R_elbow, R_wrist, L_elbow, L_wrist)
 
     # the radius thing might need calibration [ for now the value is 10 pixel ]  
-    if ( x + y ) < pow(10, 2):
-
-        # check the hand location is it above the head or inside the body
-        if nose[1] > midRightHand[1]:
-            return "HandAboveHead"
-        elif  neck[1] < midRightHand[1] and midRightHand[1] < midHip[1]:
-            return "HandWithinBody"
+    if x != 0 and y != 0:
+        maxY = max(R_elbow[1], R_wrist[1])
+        minY = min(R_elbow[1], R_wrist[1])
+        maxX = max(R_elbow[0], R_wrist[0])
+        minX = min(R_elbow[0], R_wrist[0])
+        if maxY > y and y > minY and maxX > x and x > minX:
+            # check the hand location is it above the head or inside the body
+            if nose[1] > y:
+                return "HandAboveHead"
+            elif neck[1] < y and y < midHip[1]:
+                return "HandWithinBody"
 
     return None
 
@@ -116,21 +129,27 @@ def detectGesture(bodyKeyPoints):
         else:
             return "Recording"
         pass
-    else:
-        check_right = __checkHandAboveShoulderStyle__(R_wrist, R_elbow, R_shoulder, "leftDrone", "rightDrone")
-        check_left = __checkHandAboveShoulderStyle__(L_wrist, L_elbow, L_shoulder, "forwardDrone", "backwardDrone")
 
-        # only one gesture at a time is allowed - either right or left
-        if check_right != None and check_left != None:
-            return None
-        elif check_right != None:
-            return check_right
-        elif check_left != None:
-            return check_left
-        else:
-            return None
-            #pass
+    check_right = __checkHandAboveShoulderStyle__(R_wrist, R_elbow, R_shoulder, "leftDrone", "rightDrone")
+    check_left = __checkHandAboveShoulderStyle__(L_wrist, L_elbow, L_shoulder, "forwardDrone", "backwardDrone")
 
-        pass
+    # only one gesture at a time is allowed - either right or left
+    if check_right != None and check_left != None:
+        return None
+    elif check_right != None:
+        return check_right
+    elif check_left != None:
+        return check_left
 
+    # next gesture
+
+    return None
+
+
+if __name__=="__main__":
+    #x, y = __checkingIntersectionPoint__([0,3],[1,5],[0,7],[1,6.5])
+    x, y = __checkingIntersectionPoint__([0,3],[1,5],[0,3],[1,5])
+    
+    print(x)
+    print(y)
     pass
